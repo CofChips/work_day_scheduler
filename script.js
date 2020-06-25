a1da9f44e45fcda67446cc604284b0649cd5b0e4
CofChips <qiwei.mod@gmail.com>
2020-06-25 15:01:38 -0700

    updates to improve code efficieny

--- script.js
+++ script.js
@@ -4,9 +4,10 @@ $(document).ready(function () {
 
     console.log(moment().format());
 
+    // shows today's date
     var today = $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
-
+    // div for calendar task entries
     var nineAm = $("#nineAm");
     var tenAm = $("#tenAm");
     var elevenAm = $("#elevenAm");
@@ -19,6 +20,7 @@ $(document).ready(function () {
 
     var allTimes = [nineAm, tenAm, elevenAm, twelvePm, onePm, twoPm, threePm, fourPm, fivePm]
 
+    // data-value to help identify the upper limit of each hour
     var nineAmSeconds = $("#nineAm").attr("data-value");
     var tenAmSeconds = $("#tenAm").attr("data-value");
     var elevenAmSeconds = $("#elevenAm").attr("data-value");
@@ -31,6 +33,7 @@ $(document).ready(function () {
 
     var allTimesSeconds = [nineAmSeconds, tenAmSeconds, elevenAmSeconds, twelvePmSeconds, onePmSeconds, twoPmSeconds, threePmSeconds, fourPmSeconds, fivePmSeconds]
 
+    // text area where user inputs tasks
     var formNine = $("#formNine");
     var formTen = $("#formTen");
     var formEleven = $("#formEleven");
@@ -43,6 +46,7 @@ $(document).ready(function () {
 
     var allForm = [formNine, formTen, formEleven, formTwelve, formOne, formTwo, formThree, formFour, formFive]
 
+    // save buttons
     var buttonNine = $("#buttonNine");
     var buttonTen = $("#buttonTen");
     var buttonEleven = $("#buttonEleven");
@@ -53,6 +57,9 @@ $(document).ready(function () {
     var buttonFour = $("#buttonFour");
     var buttonFive = $("#buttonFive");
 
+    var allButton = [buttonNine,buttonTen,buttonEleven,buttonTwelve,buttonOne,buttonTwo,buttonThree,buttonFour,buttonFive]
+
+    // 
     var textNine = $("#nineAmText");
     var textTen = $("#tenAmText");
     var textEleven = $("#elevenAmText");
@@ -90,253 +97,123 @@ $(document).ready(function () {
     var allKey = [keyNine, keyTen, keyEleven, keyTwelve, keyOne, keyTwo, keyThree, keyFour, keyFive];
 
     init();
-    // init2();
 
     // populates tasks with localStorage if there are values
     function init() {
-        // for (var i = 0; i < allText.length; i++) {
-        //     $(allText[i]).text("");
-        // }
-
-        var storedtask = localStorage.getItem(allKey[0]);
-        if (storedtask !== null) {
-            allTask[0] = storedtask;
-            $(allForm[0]).text(allTask[0]);
-        }
-        var storedtask = localStorage.getItem(allKey[1]);
-        if (storedtask !== null) {
-            allTask[1] = storedtask;
-            console.log(taskNine);
-            $(allForm[1]).text(allTask[1]);
-        }
-        var storedtask = localStorage.getItem(allKey[2]);
-        if (storedtask !== null) {
-            allTask[2] = storedtask;
-            $(allForm[2]).text(allTask[2]);
-        }
-        var storedtask = localStorage.getItem(allKey[3]);
-        if (storedtask !== null) {
-            allTask[3] = storedtask;
-            $(allForm[3]).text(allTask[3]);
+        for (var i = 0; i < allForm.length; i++) {
+            var storedtask = localStorage.getItem(allKey[i]);
+            if (storedtask !== null) {
+                allTask[i] = storedtask;
+                $(allForm[i]).text(allTask[i]);
+            }
         }
 
-        var storedtask = localStorage.getItem(allKey[4]);
-        if (storedtask !== null) {
-            allTask[4] = storedtask;
-            $(allForm[4]).text(allTask[4]);
+
+
+
+        // dynamically updates classes for time elements
+        let updateSeconds = function () {
+            let todayHours = moment().hour();
+            let todaySeconds = todayHours * 60 * 60;
+            let thisSecond = moment().second();
+            let sumSeconds = thisSecond + todaySeconds;
+            for (var i = 0; i < allTimesSeconds.length; i++) {
+                if (allTimesSeconds[i] < sumSeconds) {
+                    $(allTimes[i]).addClass("past");
+                    $(allTimes[i]).removeClass("present future");
+                }
+                else if (allTimesSeconds[i] >= sumSeconds && allTimesSeconds[i] - 3599 <= sumSeconds) {
+                    $(allTimes[i].addClass("present"));
+                    $(allTimes[i].removeClass("past future"));
+                }
+                else {
+                    $(allTimes[i].addClass("future"));
+                    $(allTimes[i].removeClass("past present"));
+                }
+            }
         }
 
-        var storedtask = localStorage.getItem(allKey[5]);
-        if (storedtask !== null) {
-            allTask[5] = storedtask;
-            $(allForm[5]).text(allTask[5]);
-        }
+        // listener to save task entries
+        $(".saveBtn").on("click", function(){
+            var formID = $(this).data("id");
+            var keyID = $(this).data("key");
+            // `#${formID}` === "#" + formID
+            var task = $(`#${formID}`).val();
+            localStorage.setItem(keyID, task);
+            init();
+        })
 
-        var storedtask = localStorage.getItem(allKey[6]);
-        if (storedtask !== null) {
-            allTask[6] = storedtask;
-            $(allForm[6]).text(allTask[6]);
-        }
 
-        var storedtask = localStorage.getItem(allKey[7]);
-        if (storedtask !== null) {
-            allTask[7] = storedtask;
-            $(allForm[7]).text(allTask[7]);
-        }
+        // $(buttonNine).on("click", function () {
+        //     event.preventDefault();
+        //     var task = $(formNine).val();
+        //     localStorage.setItem("taskNine", task);
+        //     init();
+        // })
 
-        var storedtask = localStorage.getItem(allKey[8]);
-        if (storedtask !== null) {
-            allTask[8] = storedtask;
-            $(allForm[8]).text(allTask[8]);
-        }
+
+        // $(buttonTen).on("click", function () {
+        //     event.preventDefault();
+        //     var task = $(formTen).val();
+        //     localStorage.setItem("taskTen", task);
+        //     init();
+        // })
+
+        // $(buttonEleven).on("click", function () {
+        //     event.preventDefault();
+        //     var task = $(formEleven).val();
+        //     localStorage.setItem("taskEleven", task);
+        //     init();
+        // })
+
+        // $(buttonTwelve).on("click", function () {
+        //     event.preventDefault();
+        //     var task = $(formTwelve).val();
+        //     localStorage.setItem("taskTwelve", task);
+        //     init();
+        // })
+
+        // $(buttonOne).on("click", function () {
+        //     event.preventDefault();
+        //     var task = $(formOne).val();
+        //     localStorage.setItem("taskOne", task);
+        //     init();
+        // })
+
+        // $(buttonTwo).on("click", function () {
+        //     event.preventDefault();
+        //     var task = $(formTwo).val();
+        //     localStorage.setItem("taskTwo", task);
+        //     init();
+        // })
+
+        // $(buttonThree).on("click", function () {
+        //     event.preventDefault();
+        //     var task = $(formThree).val();
+        //     localStorage.setItem("taskThree", task);
+        //     init();
+        // })
+
+        // $(buttonFour).on("click", function () {
+        //     event.preventDefault();
+        //     var task = $(formFour).val();
+        //     localStorage.setItem("taskFour", task);
+        //     init();
+        // })
+
+        // $(buttonFive).on("click", function () {
+        //     event.preventDefault();
+        //     var task = $(formFive).val();
+        //     localStorage.setItem("taskFive", task);
+        //     init();
+        // })
+
+
+
+        updateSeconds();
+
+        setInterval(updateSeconds, 1000);
+
+
     }
-
-    // function init2() {
-    //     var storedtask = localStorage.getItem(allKey[1]);
-    //     if (storedtask !== null) {
-    //         allTask[1] = storedtask;
-    //         console.log(taskNine);
-    //         $(allText[1]).text(allTask[1]);
-    //     }
-    // }
-
-
-
-
-
-
-
-    console.log(allKey[0]);
-
-    console.log(taskNine);
-
-
-    // console.log(allTimesSeconds[1]);
-
-
-// dynamically updates classes for time elements
-    let updateSeconds = function () {
-        let todayHours = moment().hour();
-        let todaySeconds = todayHours * 60 * 60;
-        let thisSecond = moment().second();
-        let sumSeconds = thisSecond + todaySeconds;
-        for (var i = 0; i < allTimesSeconds.length; i++) {
-            if (allTimesSeconds[i] < sumSeconds) {
-                $(allTimes[i]).addClass("past");
-                $(allTimes[i]).removeClass("present future");
-            }
-            else if (allTimesSeconds[i] >= sumSeconds && allTimesSeconds[i] - 3599 <= sumSeconds) {
-                $(allTimes[i].addClass("present"));
-                $(allTimes[i].removeClass("past future"));
-            }
-            else {
-                $(allTimes[i].addClass("future"));
-                $(allTimes[i].removeClass("past present"));
-            }
-        }
-    }
-
-    $(buttonNine).on("click", function () {
-        event.preventDefault();
-        var task = $(formNine).val();
-        localStorage.setItem("taskNine", task);
-        init();
-        // $(formNine).val("");
-    })
-
-
-    $(buttonTen).on("click", function () {
-        event.preventDefault();
-        var task = $(formTen).val();
-        localStorage.setItem("taskTen", task);
-        init();
-        // $(formTen).val("");
-    })
-
-    $(buttonEleven).on("click", function () {
-        event.preventDefault();
-        var task = $(formEleven).val();
-        localStorage.setItem("taskEleven", task);
-        init();
-        // $(formEleven).val("");
-    })
-
-    $(buttonTwelve).on("click", function () {
-        event.preventDefault();
-        var task = $(formTwelve).val();
-        localStorage.setItem("taskTwelve", task);
-        init();
-        // $(formTwelve).val("");
-    })
-
-    $(buttonOne).on("click", function () {
-        event.preventDefault();
-        var task = $(formOne).val();
-        localStorage.setItem("taskOne", task);
-        init();
-        // $(formOne).val("");
-    })
-
-    $(buttonTwo).on("click", function () {
-        event.preventDefault();
-        var task = $(formTwo).val();
-        localStorage.setItem("taskTwo", task);
-        init();
-        // $(formTwo).val("");
-    })
-
-    $(buttonThree).on("click", function () {
-        event.preventDefault();
-        var task = $(formThree).val();
-        localStorage.setItem("taskThree", task);
-        init();
-        // $(formThree).val("");
-    })
-
-    $(buttonFour).on("click", function () {
-        event.preventDefault();
-        var task = $(formFour).val();
-        localStorage.setItem("taskFour", task);
-        init();
-        // $(formFour).val("");
-    })
-
-    $(buttonFive).on("click", function () {
-        event.preventDefault();
-        var task = $(formFive).val();
-        localStorage.setItem("taskFive", task);
-        init();
-        // $(formFive).val("");
-    })
-
-    // $(formNine).on("click", function () {
-    //     event.preventDefault();
-    //     $(textNine).text("");
-    // })
-
-    // $(formTen).on("click", function () {
-    //     event.preventDefault();
-    //     $(textTen).text("");
-    // })
-
-    // $(formEleven).on("click", function () {
-    //     event.preventDefault();
-    //     $(textEleven).text("");
-    // })
-
-    // $(formTwelve).on("click", function () {
-    //     event.preventDefault();
-    //     $(textTwelve).text("");
-    // })
-
-    // $(formOne).on("click", function () {
-    //     event.preventDefault();
-    //     $(textOne).text("");
-    // })
-
-    // $(formTwo).on("click", function () {
-    //     event.preventDefault();
-    //     $(textTwo).text("");
-    // })
-
-    // $(formThree).on("click", function () {
-    //     event.preventDefault();
-    //     $(textThree).text("");
-    // })
-
-    // $(formFour).on("click", function () {
-    //     event.preventDefault();
-    //     $(textFour).text("");
-    // })
-
-    // $(formFive).on("click", function () {
-    //     event.preventDefault();
-    //     $(textFive).text("");
-    // })
-
-
-
-    // function repopulateTask() {
-    //     // formNine = "";
-    //     // // console.log(highScoreList);
-    //     // // for (var i = 0; i < highScoreList.length; i++) {
-    //     // //     var currentIndex = highScoreList[i];
-    //     // //     var li = document.createElement("li");
-    //     // //     li.innerHTML = currentIndex.initials + " - " + highScoreList[i].score;
-    //     // //     highScoreListEl.append(li);
-    //     // for( var i = 0; i< allTask.length; i++){
-    //         $(allText[0]).text(allTask[0]);
-    //     // }
-
-
-
-    // }
-
-
-    updateSeconds();
-
-    setInterval(updateSeconds, 1000);
-
-
 })
-
